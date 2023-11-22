4617fe6ea4deec36b710cf037eb1c74813d63a67
josfam <josephamukun@tutanota.com>
2023-11-22 21:21:26 +0300

    Add first MDN JavaScript literals

---
+++ javascript/strings/script.js
@@ -0,0 +1,13 @@
+const string = 'The revolution will not be televised.';
+console.log(string);
+
+/* Embedding JavaScript */
+const name = 'Chris';
+const greeting = `Hello, ${name}`;
+console.log(greeting);
+
+/*Join two varables with embedding */
+const one = "Hello, ";
+const two = "how are you?";
+const joined = `${one}${two}`;
+console.log(joined);
