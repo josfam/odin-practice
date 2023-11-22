ec3d503c5d0ee56f530611132085a5d568651eb6
josfam <josephamukun@tutanota.com>
2023-11-22 20:40:13 +0300

    Ad code for JavaScript assignment

---
+++ javascript/assignment/script.js
@@ -0,0 +1,33 @@
+/* Try the following exercises (and don’t forget to use console.log()!): */
+
+/* 1. Add 2 numbers together! (just type console.log(23 + 97) into your html file) */
+console.log(1000 + 111);
+
+/* 2. Add a sequence of 6 different numbers together. */
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+
+/*
+Print the solution to the following equation: (4 + 6 + 9) / 77
+	Answer should be approximately 0.24675
+*/
+console.log((4 + 6 + 9) / 77);
+
+/*
+Let’s use variables!
+	Type this statement at the top of the script tag: let a = 10
+	In the console console.log(a) should print 10
+	Try the following in the console: 9 * a
+	and this: let b = 7 * a (returns undefined *) and then console.log(b)
+*/
+
+/*
+You should be getting the hang of this by now… try this sequence:
+	Declare a constant variable MAX with the value 57
+	Set another variable actual to MAX - 13
+	Set another variable percentage to actual / MAX
+	If you type percentage in the console and press Enter you should see a value like 0.7719
+*/
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log(percentage);
