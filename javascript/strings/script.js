731f14bf4e0548652f6e49bbb70cc3f2a02e78d2
josfam <josephamukun@tutanota.com>
2023-11-22 21:46:24 +0300

    Add concatenation button example from MDN

--- javascript/strings/script.js
+++ javascript/strings/script.js
@@ -11,3 +11,14 @@ const one = "Hello, ";
 const two = "how are you?";
 const joined = `${one}${two}`;
 console.log(joined);
+
+/* Concatenation in context (MDN string example) */
+const button = document.querySelector('button');
+
+function greet() {
+	const name = prompt('What is your name?');
+	const greeting = document.querySelector('#greeting');
+	greeting.textContent = `Hello ${name}, nice to see you!`;
+}
+
+button.addEventListener("click", greet);
